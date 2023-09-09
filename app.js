9b18c77ac2e6adcde2cebe3221dab06808eee334
Michael Victor <radmakson@gmail.com>
2023-09-10 00:12:44 +0100

    js: functionalities

---
+++ app.js
@@ -0,0 +1,14 @@
+// Global Variables
+const getTaskInput = document.getElementById("taskInput").value;
+const pushBtn = document.getElementById("push");
+
+pushBtn.addEventListener('click', () => {
+    if(getTaskInput == 0) {
+        alert('Please Input Something');
+        console.log('I dey work');
+    } else {
+
+    }
+})
+
+// alert('hello word');
