cbbec88f4f1ac3b6efc5dfb71f65005faf065615
Michael Victor <radmakson@gmail.com>
2023-09-16 19:49:12 +0100

    init

--- app.js
+++ app.js
@@ -5,6 +5,23 @@ const taskLists = document.getElementById("tasks");
 // Load existing tasks from local storage, or initialize as an empty array
 let tasks = JSON.parse(localStorage.getItem('todos')) || [];
 
+// Event listener for adding a new task
+pushBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const inputValue = getTaskInput.value;
+
+    if (inputValue.length == 0) {
+        alert('Comrade Please Input Something');
+    } else {
+        // Add the new task to the list
+        tasks.push(inputValue);
+        localStorage.setItem('todos', JSON.stringify(tasks));
+        renderTasks(); // Re-render tasks after adding
+    }
+
+    getTaskInput.value = '';
+});
+
 // Function to render tasks
 function renderTasks() {
     taskLists.innerHTML = '';
@@ -12,7 +29,9 @@ function renderTasks() {
         const taskElement = document.createElement('div');
         taskElement.classList.add('task');
         taskElement.innerHTML = `
-            <span class="taskname">${task}</span>
+            <span class="taskname">
+                ${task}
+            </span>
             <button class="delete">
                 <i class="far fa-trash-alt"></i>
             </button>
@@ -38,20 +57,3 @@ function renderTasks() {
 
 // Initial rendering of tasks
 renderTasks();
-
-// Event listener for adding a new task
-pushBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-    const inputValue = getTaskInput.value;
-
-    if (inputValue.length == 0) {
-        alert('Comrade Please Input Something');
-    } else {
-        // Add the new task to the list
-        tasks.push(inputValue);
-        localStorage.setItem('todos', JSON.stringify(tasks));
-        renderTasks(); // Re-render tasks after adding
-    }
-
-    getTaskInput.value = '';
-});
